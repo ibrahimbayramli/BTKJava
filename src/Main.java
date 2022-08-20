9d7187d85582aa22ed0d7d6f5f7041d076177661
İbrahim BAYRAMLI <ibrahimbayramli1902@gmail.com>
2022-08-20 21:20:19 +0300

    BtkSpring

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Main {
 
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws SQLException {
         Connection connection=null;
         DbHelper dbHelper=new DbHelper();
         Statement statement =null;
@@ -12,17 +12,13 @@ public class Main {
 
         try {
             connection=dbHelper.getConnection();
-            statement=connection.createStatement();
-            resultSet=statement.executeQuery("SELECT * FROM city WHERE city_id>=450");
-            ArrayList<City> cities=new ArrayList<City>();
+            statement=connection.prepareStatement("INSERT INTO city (city_id,city,country_id,last_update) " +
+                    "VALUES (601,'Kocaeli',110,'2006-02-15 09:45:25.000000')");
+            statement.executeUpdate("INSERT INTO city (city_id,city,country_id,last_update) " +
+                    "VALUES (601,'Kocaeli',110,'2006-02-15 09:45:25.000000')");
+            System.out.println("Guncellendi");
 
-            while (resultSet.next()){
-                cities.add(new City(resultSet.getString("city_id"),
-                        resultSet.getString("city"),
-                        resultSet.getString("country_id"),
-                        resultSet.getString("last_update")));
-            }
-            System.out.println(cities.size());
+
         }catch (SQLException e){
             dbHelper.showErrorMessage(e);
         }
